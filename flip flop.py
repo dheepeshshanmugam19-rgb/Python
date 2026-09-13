5a5408f7e0c33f86c80ce8cb270fdb3ee9d6caf7
dheepeshshanmugam19-rgb <dheepeshshanmugam19@gmail.com>
2026-09-13 10:09:36 +0530

    Add files via upload

---
+++ flip flop.py
@@ -0,0 +1,14 @@
+def pallind(r):
+    e=len(r)-1
+    s=0
+    while (s<e):
+        if(r[s]!=r[e]):
+            return False
+        s+=1
+        e-=1
+    return True
+r=(1,2,3,3,2,2)
+if(pallind(r)):
+    print("The Tuple is Flip-Flop")
+else:
+    print("The Tuple is not Flip-Flop")
